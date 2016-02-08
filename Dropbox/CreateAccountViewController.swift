b1a5b730d0f73bede02507b6c7bfe9df6b931fe4
Emma Guo <emmahongguo@gmail.com>
2016-02-07 21:58:15 -0800

    Initial commit

---
+++ Dropbox/CreateAccountViewController.swift
@@ -0,0 +1,41 @@
+//
+//  CreateAccountViewController.swift
+//  Dropbox
+//
+//  Created by Emma Guo on 2/6/16.
+//  Copyright © 2016 emmaguo. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onButton(sender: AnyObject) {
+        navigationController!.popViewControllerAnimated(true)
+    }
+
+    @IBAction func didTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
